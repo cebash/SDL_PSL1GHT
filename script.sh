d9b9b1d90f11a0ffae4d2ec476ba682bebc316b9
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2011-06-01 00:56:09 -0400

    Install SDL in the right directory

--- script.sh
+++ script.sh
@@ -9,9 +9,8 @@ fi
 ./autogen.sh
 
 ## Configure the build.
-AR="ppu-ar" CC="ppu-gcc" CFLAGS="-O2 -Wall" LDFLAGS="-L$PSL1GHT/ppu/lib -lrt -llv2" RANLIB="ppu-ranlib" ./configure \
+AR="ppu-ar" CC="ppu-gcc" CFLAGS="-O2 -Wall -I$PSL1GHT/ppu/include" LDFLAGS="-L$PSL1GHT/ppu/lib -lrt -llv2" RANLIB="ppu-ranlib" ./configure \
 	--prefix="$PS3DEV/portlibs/ppu" --host=ppu-psl1ght \
-	--includedir="$PSL1GHT/ppu/include" --libdir="$PSL1GHT/ppu/lib" \
 	--enable-atomic=yes --enable-video-psl1ght=yes --enable-joystick=yes --enable-audio=yes\
 	|| { exit 1; }
 
