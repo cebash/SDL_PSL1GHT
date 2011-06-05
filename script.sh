6478e9615694e2aee107effcbdf3d7814dde5583
Marcus Comstedt <marcus@mc.pp.se>
2011-06-05 20:26:05 +0200

    Updated for new ps3toolchain

--- script.sh
+++ script.sh
@@ -9,8 +9,8 @@ fi
 ./autogen.sh
 
 ## Configure the build.
-AR="ppu-ar" CC="ppu-gcc" CFLAGS="-O2 -Wall -I$PSL1GHT/ppu/include" LDFLAGS="-L$PSL1GHT/ppu/lib -lrt -llv2" RANLIB="ppu-ranlib" ./configure \
-	--prefix="$PS3DEV/portlibs/ppu" --host=ppu-psl1ght \
+CFLAGS="-O2 -Wall -I$PSL1GHT/ppu/include" LDFLAGS="-L$PSL1GHT/ppu/lib -lrt -llv2" ./configure \
+	--prefix="$PS3DEV/portlibs/ppu" --host=powerpc64-ps3-elf \
 	--enable-atomic=yes --enable-video-psl1ght=yes --enable-joystick=yes --enable-audio=yes\
 	|| { exit 1; }
 
