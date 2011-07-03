8f7296955ffaa9282e2d3bb2370eb9a266431f53
Marcus Comstedt <marcus@mc.pp.se>
2011-07-03 11:30:24 +0200

    JANITORIAL: Fix SDL_revision.h generation
    Remove the committed SDL_revision.h to prevent it always showing up as
    modified.  Instead, generate the default contents in updaterev.sh as
    needed.  Modify showrev.sh to generate a better commit id, which includes
    a commit sequence number.  Modify updaterev.sh to be able to extract this
    number for the SDL_REVISION_NUMBER define.

--- build-scripts/showrev.sh
+++ build-scripts/showrev.sh
@@ -4,4 +4,4 @@
 
 # FIXME: this prints the tip, which isn't useful if you're on a different
 #  branch, or just not sync'd to the tip.
-hg tip --template 'hg-{rev}:{node|short}' ||  git show | head -n1 | sed 's/commit //' || (echo "hg-0:baadf00d"; exit 1)
+hg tip --template 'hg-{rev}:{node|short}' || git describe --tags --match=BEGIN --always | sed -e 's/^BEGIN-/git-/' || (echo "hg-0:baadf00d"; exit 1)
--- build-scripts/updaterev.sh
+++ build-scripts/updaterev.sh
@@ -7,9 +7,15 @@ cd `dirname $0`
 srcdir=..
 header=$outdir/include/SDL_revision.h
 
+if [ -f $header ]; then :; else
+    cat >$header <<EOF
+#define SDL_REVISION "hg-0:aaaaaaaaaaah"
+#define SDL_REVISION_NUMBER 0
+EOF
+fi
 rev=`sh showrev.sh 2>/dev/null`
 if [ "$rev" != "" -a "$rev" != "hg-0:baadf00d" ]; then
-    revnum=`echo $rev | sed 's,hg-\([0-9]*\).*,\1,'`
+    revnum=`echo $rev | sed 's,\(hg\|git\)-\([0-9]*\).*,\2,'`
     echo "#define SDL_REVISION \"$rev\"" >$header.new
     echo "#define SDL_REVISION_NUMBER $revnum" >>$header.new
     if diff $header $header.new >/dev/null 2>&1; then
